c69e002f3f2b392b12fad9174da2f46284e54399
himanshusharma16 <sharma.himanshu1836@gmail.com>
2025-03-23 19:17:07 +0800

    adding handler test

--- src/test/java/cinemaBooking/CinemaBookingTest.java
+++ src/test/java/cinemaBooking/CinemaBookingTest.java
@@ -1,20 +1,62 @@
 package cinemaBooking;
 
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 public class CinemaBookingTest {
 
-    private CinemaBooking cinemaBooking;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+    private final InputStream originalIn = System.in;
+    private ByteArrayInputStream in;
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+        System.setIn(originalIn);
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
 
     @Test
-    public void whenIncorrectInputGiven_errorMessageIsReturned(){
-
+    public void whenIncorrectInputGiven_errorMessageIsPrinted(){
+        in = new ByteArrayInputStream(("ABCDABCD" + System.lineSeparator() + "1").getBytes());
+        System.setIn(in);
+        CinemaBooking.main();
+        Assertions.assertTrue(outContent.toString().contains(CinemaBooking.INCORRECT_DATA));
     }
 
     @Test
-    public void whenCorrectInputGiven_hallObjectIsCreatedWithCorrectData(){
+    public void whenIncorrectInputGiven_afterChoosingExit_goodbyeMessageIsPrinted(){
+        in = new ByteArrayInputStream(("ABCDABCD" + System.lineSeparator() + "0").getBytes());
+        System.setIn(in);
+        CinemaBooking.main();
+        Assertions.assertTrue(outContent.toString().contains("Thank you for using GIC Cinemas system. Bye!"));
+    }
 
+    @Test
+    public void whenCorrectInputGiven_noErrorInConsole(){
+        in = new ByteArrayInputStream(("Inception 5 10" + System.lineSeparator() + "0").getBytes());
+        System.setIn(in);
+        CinemaBooking.main();
+        Assertions.assertFalse(outContent.toString().contains("Thank you for using GIC Cinemas system. Bye!"));
+        Assertions.assertFalse(outContent.toString().contains(CinemaBooking.INCORRECT_DATA));
     }
 
 }
---
+++ src/test/java/cinemaBooking/handler/BookingFlowHandlerTest.java
@@ -0,0 +1,55 @@
+package cinemaBooking.handler;
+
+import cinemaBooking.model.CinemaHall;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ClassLoaderUtils;
+
+import java.io.*;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BookingFlowHandlerTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+    private final InputStream originalIn = System.in;
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+        System.setIn(originalIn);
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @Test
+    public void whenSelectExitFromWelcomeMenu_emitsGoodbyeMessage() {
+        ByteArrayInputStream in = new ByteArrayInputStream("3".getBytes());
+        System.setIn(in);
+        Scanner myScanner = new Scanner(in);
+        var handler = new BookingFlowHandler(myScanner);
+        var hall = new CinemaHall(2,2);
+        handler.handle(hall);
+        assertTrue(outContent.toString().contains("Thank you for using GIC Cinemas system. Bye!"));
+    }
+
+    @Test
+    public void whenInvalidInputInWelcomeMenu_emitsErrorMessage() {
+        InputStream inputStream = ClassLoaderUtils.getDefaultClassLoader().getResourceAsStream("test.txt");
+        Scanner myScanner = new Scanner(inputStream);
+        var handler = new BookingFlowHandler(myScanner);
+        var hall = new CinemaHall(2,2);
+        handler.handle(hall);
+        assertTrue(outContent.toString().contains("Wrong input. Please try again!!"));
+    }
+}
